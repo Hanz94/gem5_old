b9ec814a5dd4e344237cf9ce05ff546156d7bb36
Hansika <hansikam94@gmail.com>
2022-08-13 17:34:08 -0400

    example run command for script

--- gem5/scripts/transform_data_to_numpy.py
+++ gem5/scripts/transform_data_to_numpy.py
@@ -1,3 +1,6 @@
+# TO RUN : refer example caommand below
+# python3 transform_data_to_numpy.py --no-of-nodes=64_nodes_85__0.001 --base-path=/home/hansika/gem5/gem5/scripts/ --cal-dir-path=calculated_test/ --rawd-dir-path=dummy_raw_data/ --numpy-dir-path=numpy_data_test/
+
 import glob
 import os
 import numpy as np
